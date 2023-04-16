87a98d7278020676e1624851e86b6bd36d828b70
Milda Aditia Putra <aditia.putra100@gmail.com>
2023-04-16 15:00:23 +0700

    add event handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,38 @@ import "./App.css";
 import Button from "@mui/material/Button/Button";
 import Box from "@mui/material/Box/Box";
 import ImageDrop from "./component/ImageDrop";
+import { useState, useCallback, useEffect } from "react";
 
 function App() {
+  const [file, setFile] = useState<File>();
+
+  const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const imageFiles = event.target.files;
+
+    if (!imageFiles) return;
+
+    setFile(imageFiles[0]);
+  };
+
+  useEffect(() => {
+    console.log(file);
+  }, []);
   return (
     <div className="App">
       <h2 className="text-center">Upload your image</h2>
       <p className="annotation text-center">File should be Jpeg, Png,...</p>
-      <ImageDrop />
+      <ImageDrop setFile={setFile} />
       <p className="describe text-center">Or</p>
       <Box textAlign={"center"}>
         <Button variant="contained" component="label">
           Choose a file
-          <input hidden accept="image/*" multiple type="file" />
+          <input
+            hidden
+            accept="image/*"
+            multiple
+            type="file"
+            onChange={handleFileInput}
+          />
         </Button>
       </Box>
     </div>
--- src/component/ImageDrop.tsx
+++ src/component/ImageDrop.tsx
@@ -1,20 +1,61 @@
 import { Box } from "@mui/material";
 import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
+import { useState } from "react";
 
-function ImageDrop() {
+const styles = {
+  boxImageIdle: {
+    background: "#F6F8FB",
+    border: "1px dashed #97BEF4",
+    borderRadius: 12,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "2em",
+  },
+  boxImageOnDrag: {
+    background: "#F6F8FB",
+    border: "1px dashed #1976d2",
+    borderRadius: 12,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "2em",
+  },
+};
+
+type StateFile = {
+  setFile: React.Dispatch<React.SetStateAction<File | undefined>>;
+};
+
+function ImageDrop({ setFile }: StateFile) {
+  const [drag, setDrag] = useState(false);
+
+  const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setDrag(true);
+  };
+
+  const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+    setDrag(false);
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const imageFiles = event.dataTransfer.files;
+    setFile(imageFiles[0]);
+    setDrag(false);
+  };
+
   return (
     <Box
-      sx={{
-        background: "#F6F8FB",
-        border: "1px dashed #97BEF4",
-        borderRadius: 12,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "2em",
-      }}
-      onDragOver={() => console.log("huuh")}
+      id="imageDnD"
+      sx={!drag ? styles.boxImageIdle : styles.boxImageOnDrag}
+      draggable={true}
+      onDragOver={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
     >
       <InsertPhotoIcon sx={{ fontSize: 150, color: "#828282" }} />
       <p className="describe">Drag & Drop your image here</p>
